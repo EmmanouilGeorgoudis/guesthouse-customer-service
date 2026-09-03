f572d3eb49022669a02089974b9e3da59f19c6d4
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2026-09-03 12:53:41 +0200

    delete lombok

--- src/main/java/manolis/guesthousecustomerservice/dtos/CustomerDTO.java
+++ src/main/java/manolis/guesthousecustomerservice/dtos/CustomerDTO.java
@@ -1,13 +1,31 @@
 package manolis.guesthousecustomerservice.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CustomerDTO {
     private String name;
     private Long id;
+
+    public CustomerDTO() {
+    }
+
+    public CustomerDTO(String name, Long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
