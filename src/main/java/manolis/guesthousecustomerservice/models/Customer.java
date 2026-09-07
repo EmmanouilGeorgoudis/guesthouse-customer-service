f931188e10503321ffb16cb84d79ae8f5fd979ed
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2026-09-07 10:05:51 +0200

    replace lombok with current code

--- src/main/java/manolis/guesthousecustomerservice/models/Customer.java
+++ src/main/java/manolis/guesthousecustomerservice/models/Customer.java
@@ -3,26 +3,41 @@ package manolis.guesthousecustomerservice.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
+
 @Entity
 public class Customer {
 
     @Id
     @GeneratedValue
     private Long id;
-    @NotBlank
     private String name;
 
+    public Customer() {
+    }
+
+    public Customer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Customer(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
--- src/main/java/manolis/guesthousecustomerservice/services/impl/CustomerServiceImpl.java
+++ src/main/java/manolis/guesthousecustomerservice/services/impl/CustomerServiceImpl.java
@@ -2,10 +2,12 @@ package manolis.guesthousecustomerservice.services.impl;
 
 
 
-import com.example.guesthousebookingsystem.models.Customer;
-import com.example.guesthousebookingsystem.repositories.BookingRepository;
-import com.example.guesthousebookingsystem.repositories.CustomerRepository;
-import com.example.guesthousebookingsystem.services.CustomerService;
+
+
+import manolis.guesthousecustomerservice.dtos.CustomerDTO;
+import manolis.guesthousecustomerservice.models.Customer;
+import manolis.guesthousecustomerservice.repositories.CustomerRepository;
+import manolis.guesthousecustomerservice.services.CustomerService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,11 +18,10 @@ import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
     private final CustomerRepository customerRepository;
-    private final BookingRepository bookingRepository;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository, BookingRepository bookingRepository) {
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
-        this.bookingRepository = bookingRepository;
     }
 
     @Override
@@ -45,13 +46,13 @@ public class CustomerServiceImpl implements CustomerService {
         customerRepository.save(customer);
 
     }
-    @Override
-    public void delete(Long id) {
-        if (bookingRepository.existsByCustomerId(id)) {
-            throw new RuntimeException("Can't remove customers with active bookings!");
-        }
-        customerRepository.deleteById(id);
-    }
+//    @Override
+//    public void delete(Long id) {
+//        if (bookingRepository.existsByCustomerId(id)) {
+//            throw new RuntimeException("Can't remove customers with active bookings!");
+//        }
+//        customerRepository.deleteById(id);
+//    }
 
 
 }
